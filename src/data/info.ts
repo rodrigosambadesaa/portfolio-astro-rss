d4eadcb51d0bc17114bcc83a8e6fa41aa189a671
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2024-02-06 10:11:33 +0100

    Update info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,73 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  name: "Rodrigo Sambade",
+  jobDescription: "Mobile Application Developer. Previosuly, Research Technician at the University of Santiago de Compostela, Web Developer and Java Developer.",
+  about: `I'm a Mobile Application Developer with a strong background in Java and Web Development. I have experience working with Java, Handlebars, HTML, CSS, JavaScript. I'm also a huge fan of open-source and I have contributed to several projects.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Concello de Santiago de Compostela - CERSIA Empresa",
+      location: "Santiago de Compostela, Galicia, Spain",
+      startDate: "Oct 2023",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Developing three mobile applications for the Senda del Río Sar, the Mercado de Abstracciones and the Museo do Pobo Galego.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "University of Santiago de Compostela",
+      location: "Santiago de Compostela, Galicia, Spain",
+      startDate: "Arp 2022",
+      endDate: "Apr 2023",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "✓ Developed and administered two websites",
+        "✓ Refactored SimpleNLG-GL, a library for text generation in Galician",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Coremain",
+      location: "Santiago de Compostela, Galicia, Spain",
+      startDate: "Sep 2019",
+      endDate: "Nov 2019",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "✓ Worked in the development of Camunda BPM, a workflow automation tool, for the company's Private Industry department",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "Courses in CampusMVP",
+      location: "Online",
+      startDate: "Apr 2021",
+      endDate: "Apr 2021",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "These are the courses I have taken in CampusMVP: https://www.campusmvp.es/certificados/rodrigo-sambade-saa",
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      name: "Certificate of Higher Education: Development of Multi-Platform Applications, Tecnología de la informaciónCertificate of Higher Education: Development of Multi-Platform Applications, Information Technology",
+      location: "Santiago de Compostela, Galicia, Spain",
+      startDate: "2017",
+      endDate: "2019",
+      description: ["I have learned to develop multi-platform applications using Java and Android Studio, as well as websites."],
     },
     {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "Primary Education, Secondary Education and Baccalaureate",
+      location: "Santiago de Compostela, Galicia, Spain",
+      startDate: "1997",
+      endDate: "2013",
+      description: ["I have studied in the Colegio Manuel Peleteiro, Santiago de Compostela."],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    facebook: "https://www.facebook.com/RodrigoSambadeSaa/",
+    github: "https://github.com/rodrigosambadesaa",
+    email: "mailto:rodrigosambadesaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/rodrigo-sambade-5675bb186/",
   },
-
-  projects: [
-    {
-      title: "Astro Portfolio",
-      isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
-    },
-  ],
 };
